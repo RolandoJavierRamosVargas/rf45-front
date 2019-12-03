c6e3d596905c5853197bac8efb7a01b03402970d
Rolando Javier <rolando.ramos2@unmsm.edu.pe>
2019-12-03 10:20:32 -0500

    v1.6

--- src/Configuracion/Config.js
+++ src/Configuracion/Config.js
@@ -3,5 +3,5 @@
    // const CONFIG= 'https://sigadev1-consultarecibos-back.herokuapp.com/' 
     // const CONFIG= 'http://localhost:8080/';
       // const CONFIG= 'https://backmastersteam.herokuapp.com/';
-     const CONFIG= 'https://rf45-back1.herokuapp.com/';
+     const CONFIG= 'https://rf45-back2.herokuapp.com/';
 export default CONFIG;
